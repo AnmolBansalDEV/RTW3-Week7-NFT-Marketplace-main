8c58f1d3f588d27a4e1511c308d7bb49a81b03ef
Anmol Bansal <anmolbansal1807@gmail.com>
2024-05-06 13:36:24 +0530

    fix: check if metamask is installed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -25,7 +25,6 @@ function updateButton() {
 }
 
 async function connectWebsite() {
-
     const chainId = await window.ethereum.request({ method: 'eth_chainId' });
     if(chainId !== "0xaa36a7")
     {
@@ -45,6 +44,10 @@ async function connectWebsite() {
 }
 
   useEffect(() => {
+    if (!window.ethereum) {
+      alert('Please install MetaMask!');
+      return;
+    }
     // let val = window.ethereum.isConnected
     window.ethereum.request({method: 'eth_accounts'}).then(
      accounts => {
@@ -70,61 +73,61 @@ async function connectWebsite() {
     return (
       <div className="">
         <nav className="w-screen">
-          <ul className='flex items-end justify-between py-3 bg-transparent text-white pr-5'>
-          <li className='flex items-end ml-5 pb-2'>
+          <ul className='flex items-end justify-between py-3 pr-5 text-white bg-transparent'>
+          <li className='flex items-end pb-2 ml-5'>
             <Link to="/">
             {/* <img src={fullLogo} alt="" width={120} height={120} className="inline-block -mt-2"/> */}
-            <div className='inline-block font-bold text-xl ml-2'>
+            <div className='inline-block ml-2 text-xl font-bold'>
               AquaSavvy Marketplace
             </div>
             </Link>
           </li>
           <li className='w-2/6'>
-            <ul className='lg:flex justify-between font-bold mr-10 text-lg'>
+            <ul className='justify-between mr-10 text-lg font-bold lg:flex'>
             {location.pathname === "/GeoMap" ? 
-              <li className='border-b-2 hover:pb-0 p-2'>
+              <li className='p-2 border-b-2 hover:pb-0'>
                 <Link to="/GeoMap">GeoMap</Link>
               </li>
               :
-              <li className='hover:border-b-2 hover:pb-0 p-2'>
+              <li className='p-2 hover:border-b-2 hover:pb-0'>
                 <Link to="/GeoMap">GeoMap</Link>
               </li>              
               }
               {location.pathname === "/" ? 
-              <li className='border-b-2 hover:pb-0 p-2'>
+              <li className='p-2 border-b-2 hover:pb-0'>
                 <Link to="/">Marketplace</Link>
               </li>
               :
-              <li className='hover:border-b-2 hover:pb-0 p-2'>
+              <li className='p-2 hover:border-b-2 hover:pb-0'>
                 <Link to="/">Marketplace</Link>
               </li>              
               }
               {location.pathname === "/sellNFT" ? 
-              <li className='border-b-2 hover:pb-0 p-2'>
+              <li className='p-2 border-b-2 hover:pb-0'>
                 <Link to="/sellNFT">List Water reserve</Link>
               </li>
               :
-              <li className='hover:border-b-2 hover:pb-0 p-2'>
+              <li className='p-2 hover:border-b-2 hover:pb-0'>
                 <Link to="/sellNFT">List Water reserve</Link>
               </li>              
               }              
               {location.pathname === "/profile" ? 
-              <li className='border-b-2 hover:pb-0 p-2'>
+              <li className='p-2 border-b-2 hover:pb-0'>
                 <Link to="/profile">Profile</Link>
               </li>
               :
-              <li className='hover:border-b-2 hover:pb-0 p-2'>
+              <li className='p-2 hover:border-b-2 hover:pb-0'>
                 <Link to="/profile">Profile</Link>
               </li>              
               }  
               <li>
-                <button className="enableEthereumButton bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-sm" onClick={connectWebsite}>{connected ? "Connected":"Connect Wallet"}</button>
+                <button className="px-4 py-2 text-sm font-bold text-white bg-blue-500 rounded enableEthereumButton hover:bg-blue-700" onClick={connectWebsite}>{connected ? "Connected":"Connect Wallet"}</button>
               </li>
             </ul>
           </li>
           </ul>
         </nav>
-        <div className='text-white text-bold text-right mr-10 text-sm'>
+        <div className='mr-10 text-sm text-right text-white text-bold'>
           {currAddress !== "0x" ? "Connected to":"Not Connected. Please login to view NFTs"} {currAddress !== "0x" ? (currAddress.substring(0,15)+'...'):""}
         </div>
       </div>
